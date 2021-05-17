64ef86528ca4bf61ce23e6a823fdd9efadc66dc6
Caleb <choover11@gmail.com>
2021-05-17 12:08:25 -0700

    expanded on layout

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { FlatList, StyleSheet, Text, Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider as PaperProvider, Appbar } from 'react-native-paper';
 import NotesScreen from './screens/Notes';
 import HomeScreen from './screens/Home';
 
@@ -9,11 +9,17 @@ const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Notes">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notes" component={NotesScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <Appbar>
+        <Appbar.BackAction onPress={() => {}} />
+        <Appbar.Content title="CWU / Spring 2021 / CS446 " />
+      </Appbar>
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Notes">
+          <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen name="Notes" component={NotesScreen} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
--- screens/Notes.js
+++ screens/Notes.js
@@ -1,5 +1,19 @@
 import React from 'react';
-import { List } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { List, FAB } from 'react-native-paper';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  fab: {
+    position: 'absolute',
+    margin: '1em',
+    right: 0,
+    bottom: 0,
+  },
+})
 
 function NotesScreen ({ navigation }) {
   const items = [{
@@ -20,12 +34,23 @@ function NotesScreen ({ navigation }) {
     title: 'Lecture 4'
   }];
 
+  const createNewNote = () => {
+
+  }
+
   return (
-    <div>
-      {items.map((item) => (
-        <List.Item key={item.id} title={item.title} description={item.date.toDateString()} />
-      ))}
-    </div>
+    <View style={styles.container}>
+      <div>
+        {items.map((item) => (
+          <List.Item key={item.id} title={item.title} description={item.date.toDateString()} />
+          ))}
+      </div>
+      <FAB
+        style={styles.fab}
+        icon="plus"
+        onPress={() => createNewNote()}
+      />
+    </View>
   );
 }
 
