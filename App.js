bf56b91e193d6ef8f8f86f243e878ee1ae3f4908
Richard <82605139+rhawkens99@users.noreply.github.com>
2021-05-20 14:25:16 -0700

    Version that works with SchoolScreen and Settings

--- App.js
+++ App.js
@@ -1,25 +1,20 @@
 import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import { Provider as PaperProvider, Appbar } from 'react-native-paper';
-import NotesScreen from './screens/Notes';
-import HomeScreen from './screens/Home';
 
-const Drawer = createDrawerNavigator();
+// components/screens
+import { ThemeProvider } from './app/screens/ThemeController';
+import StartScreen from './app/screens/StartScreen';
 
-export default function App() {
+function App() {
   return (
-    <PaperProvider>
-      <Appbar>
-        <Appbar.BackAction onPress={() => {}} />
-        <Appbar.Content title="CWU / Spring 2021 / CS446 " />
-      </Appbar>
-      <NavigationContainer>
-        <Drawer.Navigator initialRouteName="Notes">
-          <Drawer.Screen name="Home" component={HomeScreen} />
-          <Drawer.Screen name="Notes" component={NotesScreen} />
-        </Drawer.Navigator>
-      </NavigationContainer>
-    </PaperProvider>
-  );
+    <ThemeProvider>
+      <PaperProvider>
+        <StartScreen />
+      </PaperProvider>
+    </ThemeProvider>
+  )
 }
+
+export default App;
