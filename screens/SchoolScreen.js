fa4b64cf30714e48a530f5671f34c13db00b0185
rhawkens99 <82605139+rhawkens99@users.noreply.github.com>
2021-05-19 19:41:57 -0700

    SchoolScreens.js
    Started trying to figure out Drawer Navigation

--- screens/SchoolScreen.js
+++ screens/SchoolScreen.js
@@ -1,61 +1,70 @@
-import React from 'react';
-import { StyleSheet,SafeAreaView, ScrollView } from 'react-native';
+
+import React, { useContext } from 'react';
+import { StyleSheet, SafeAreaView, ScrollView } from 'react-native';
 import { Appbar, FAB, IconButton, Button } from 'react-native-paper';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { ThemeContext } from './ThemeController';
 
+const Drawer = createDrawerNavigator();
 
-function SchoolScreen({navigation}) {
+function SchoolScreen({ navigation }) {
+
+  const { theme } = useContext(ThemeContext)
+
   return (
     <>
       <Appbar.Header>
         <Appbar.Content title="CWU" titleStyle={{ left: 60, fontSize: 35, fontFamily: 'sans-serif', }} />
 
         {/* settings button */}
-        <IconButton 
-          icon="cog" 
-          color="white" 
+        <IconButton
+          icon="cog"
+          color="white"
           onPress={() => navigation.navigate("Settings")}
           size={30}
-          style={{ }} 
+          style={{}}
         />
 
         {/* school select button */}
-        <IconButton 
+        <IconButton
           icon="menu"
-          color = "white"
-          onPress={() => console.log("Opens Drawer with list of schools")}
+          color="white"
+          onPress={() => console.log("Navigate Schools") /*navigation.navigate("School 2")*/}
           size={30}
           style={{ position: 'absolute', left: 0 }}
         />
-      
       </Appbar.Header>
 
       {/* list of terms with ability to scroll */}
-      <SafeAreaView style={styles.container}>
+      <SafeAreaView style={[styles.container, { backgroundColor: theme.backgroundColor }]}>
         <ScrollView style={{ flex: 1, width: 400, }} >
-          <Button mode="text" uppercase="" onPress={() => console.log("Spring 2021")} labelStyle={styles.buttonText}  style={styles.button} >
+          <Button mode="text" uppercase="" onPress={() => console.log("Spring 2021")} labelStyle={styles.buttonText} style={styles.button} >
             Spring 2021
           </Button>
 
-          <Button mode="text" uppercase="" onPress={() => console.log("Winter 2021")} labelStyle={styles.buttonText}  style={styles.button} >
+          <Button mode="text" uppercase="" onPress={() => console.log("Winter 2021")} labelStyle={styles.buttonText} style={styles.button} >
             Winter 2021
           </Button>
 
-          <Button mode="text" uppercase="" onPress={() => console.log("Fall 2020")} labelStyle={styles.buttonText}  style={styles.button} >
+          <Button mode="text" uppercase="" onPress={() => console.log("Fall 2020")} labelStyle={styles.buttonText} style={styles.button} >
             Fall 2020
           </Button>
         </ScrollView>
 
         {/* add term button */}
-        <FAB 
+        <FAB
           style={styles.fab}
           icon="plus"
-          onPress={() => console.log("Term added")} 
-        /> 
+          onPress={() => console.log("Term added")}
+        />
 
       </SafeAreaView>
     </>
   );
-}
+} // end TermSelection
+
+// var Drawer = createDrawerNavigator();
 
 const styles = StyleSheet.create({
   button: {
@@ -65,22 +74,21 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
   },
   buttonText: {
-      color: "white",
-      fontFamily: "sans-serif",
-      fontSize: 24,
-      textAlign: 'center',
-      marginTop: 10,
+    color: "white",
+    fontFamily: "sans-serif",
+    fontSize: 24,
+    textAlign: 'center',
+    marginTop: 10,
   },
   container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-    },
-  fab : {
+    flex: 1,
+    alignItems: 'center',
+  },
+  fab: {
     position: 'absolute',
     top: 650,
     right: 20,
   },
-  });
+});
 
 export default SchoolScreen;
