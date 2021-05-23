1407b26d508597d19841afdad413bb8c312a5024
Richard <82605139+rhawkens99@users.noreply.github.com>
2021-05-22 21:03:09 -0700

    Dynamically add schools and change header accordingly

--- screens/Drawer.js
+++ screens/Drawer.js
@@ -1,12 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { View } from 'react-native';
 import { DrawerItem, DrawerContentScrollView } from '@react-navigation/drawer';
-import { Drawer as ReactNativePaperDrawer, Button } from 'react-native-paper';
+import { Drawer as ReactNativePaperDrawer, Button, TextInput } from 'react-native-paper';
 
 import { SchoolsContext } from "./SchoolsProvider";
 
 function DrawerContent() {
     const { state, dispatch } = useContext(SchoolsContext);
+    const [text, setText] = useState('');
+
+    const handleSchoolSubmit = (name) => {
+        setText('');
+        addSchool(name);
+    }
 
     function addSchool(school) {
         dispatch({ type: 'addSchool', payload: school });
@@ -22,11 +28,11 @@ function DrawerContent() {
             <View>
                 <ReactNativePaperDrawer.Section>
                     {state.schools.map((school) => (
-                        <DrawerItem key={school.name} label={school.name} /> /* function setSchool used here */
+                        <DrawerItem key={school.name} label={school.name} onPress={() => setSchool(school.name)} />
                     ))}
                 </ReactNativePaperDrawer.Section>
                 <ReactNativePaperDrawer.Section>
-                    <Button onPress={() => addSchool('UW')}>Add School</Button>
+                    <TextInput label="Add New School" value={text} onChangeText={text => setText(text)} onEndEditing={() => handleSchoolSubmit(text)} />
                 </ReactNativePaperDrawer.Section>
             </View>
         </DrawerContentScrollView>
--- screens/Header.js
+++ screens/Header.js
@@ -1,33 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Appbar } from 'react-native-paper';
+import { SchoolsContext } from './SchoolsProvider';
 
 function Header({ navigation, previous, scene }) {
-  const { route } = scene;
+    const { route } = scene;
 
-  return (
-      <Appbar.Header>
-          <Appbar.Action
-              icon="menu"
-              onPress={() => navigation.openDrawer()}
-              size={30}
-          />
-          <Appbar.Content title="CWU" titleStyle={{ left: 60, fontSize: 35, fontFamily: 'sans-serif', }} />
-          {/* settings button */}
-          {route.name === 'Settings' ?
-              <Appbar.BackAction
-                  onPress={() => navigation.navigate("School")}
-                  size={30}
-              />
-          :
-              <Appbar.Action
-                  icon="cog"
-                  onPress={() => navigation.navigate("Settings")}
-                  size={30}
-              />
-          }
+    const { state, dispatch } = useContext(SchoolsContext);
 
-      </Appbar.Header>
-  );
+    return (
+        <Appbar.Header style={{ backgroundColor: "rgb(98,0,238)", }}>
+            <Appbar.Action
+                icon="menu"
+                onPress={() => navigation.openDrawer()}
+                size={30}
+            />
+            <Appbar.Content title={state.currentSchool} titleStyle={{ left: 60, fontSize: 35, fontFamily: 'sans-serif', }} />
+            {/* settings button */}
+            {route.name === 'Settings' ?
+                <Appbar.BackAction
+                    onPress={() => navigation.navigate("School")}
+                    size={30}
+                />
+                :
+                <Appbar.Action
+                    icon="cog"
+                    onPress={() => navigation.navigate("Settings")}
+                    size={30}
+                />
+            }
+
+        </Appbar.Header>
+    );
 }
 
 export default Header;
